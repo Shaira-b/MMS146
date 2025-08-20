de84a2d440b180fd8b0d3b4604fc042ca225325f
Shaira-b <stbulan@up.edu.ph>
2025-08-20 22:43:49 +0800

    Update test_player.py

--- tests/test_player.py
+++ tests/test_player.py
@@ -3,8 +3,7 @@ import os
 import unittest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from player import Player  
+from player import Player
 
 class TestPlayer(unittest.TestCase):
     def test_get_and_set_name(self):
