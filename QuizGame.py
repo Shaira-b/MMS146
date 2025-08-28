7bda6f019f84b7bf91cb77c0df24ec892047a08a
pmtimon <120622136+pmtimon@users.noreply.github.com>
2025-08-28 15:51:32 +0800

    Update QuizGame.py

--- QuizGame.py
+++ QuizGame.py
@@ -15,16 +15,11 @@ class QuizGame:
         self.question_bank = question_bank
         self.score = 0
 
-    def shuffle_question_bank(self, category=None, difficulty=None):
+    def shuffle_question_bank(self):
         '''Randomly shuffles the list of questions available for the game. Should happen automatically when the game starts.'''
-        if category and difficulty:
-            questions = self.get_questions(category, difficulty)
-            random.shuffle(questions)
-        else:
-            for cat in self.question_bank:
-                for diff in self.question_bank[cat]:
-                    random.shuffle(self.question_bank[cat][diff])
-
+        random.shuffle(self.question_bank)
+        self.current_index = 0
+        
     def display_question(self):
         '''
         Displays a question (class) from the shuffled question bank
@@ -48,4 +43,5 @@ question3 = Question(qu = "The", a ="Thinkerrr")
 quiz1 = QuizGame(question_bank = [question1, question2, question3])
 
 print(quiz1.display_question())
+
 
